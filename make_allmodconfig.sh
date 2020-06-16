1211891113a839d49826d3c7e34b1c94c8fc2a9c
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2020-06-16 23:07:49 +0200

    Linux 5.8-rc1

--- make_allmodconfig.sh
+++ make_allmodconfig.sh
@@ -393,6 +393,7 @@ then
         echo 'CONFIG_DRM_AMD_DC_DCN2_0=y' >> "$KBUILD_OUTPUT/.config"
         echo 'CONFIG_DRM_AMD_DC_DCN2_1=y' >> "$KBUILD_OUTPUT/.config"
         echo 'CONFIG_DRM_AMD_DC_DSC_SUPPORT=y' >> "$KBUILD_OUTPUT/.config"
+        echo 'CONFIG_UBSAN_BOUNDS=y' >> "$KBUILD_OUTPUT/.config"
     fi
 
     if [ "${BUILDCONF:-}" = 'xgcc-arm' ]
